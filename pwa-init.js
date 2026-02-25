75ae36dc928e15da24677c3a02f8d92de0f0c9d8
Yevgeniy Korobka <natureinitiation@gmail.com>
2026-02-25 08:19:36 +0200

    Update pwa-init.js

--- pwa-init.js
+++ pwa-init.js
@@ -1,7 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    // Используем абсолютный путь от корня, чтобы скрипт работал из любой папки
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('Распорядитель на месте:', reg.scope))
       .catch(err => console.log('Служба дала сбой:', err));
   });
