494a48e5660f67bcf7f2b455159f688b1fc60632
Yevgeniy Korobka <natureinitiation@gmail.com>
2026-05-31 19:09:55 +0300

    Create service-life.js

---
+++ service-life.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'outside-in-planet-v1';
+const ASSETS = [
+  './',
+  './life.html',
+  './manifest-life.json',
+  './life.png',
+  'https://unpkg.com/three@0.160.0/build/three.module.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
