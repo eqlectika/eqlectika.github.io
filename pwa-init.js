e9e370189942d3abaf8861eadd7e4deae64596f7
Yevgeniy Korobka <natureinitiation@gmail.com>
2026-07-02 21:37:53 +0300

    Delete pwa-init.js

--- pwa-init.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Распорядитель на месте:', reg.scope))
-      .catch(err => console.log('Служба дала сбой:', err));
-  });
-}
