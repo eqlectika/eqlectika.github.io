51c029fce0f0fdfa92152143335ec11b45ccb13f
Yevgeniy Korobka <natureinitiation@gmail.com>
2026-08-23 21:17:27 +0300

    Change cache name and update core assets
    Updated cache name and removed references to capital assets.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'polarity-vmedia';
+const CACHE_NAME = 'polarity-vcapital';
 
 const CORE_ASSETS = [
     './',
@@ -10,7 +10,6 @@ const CORE_ASSETS = [
     './match.html',
     './life.html',
     './spark.html',
-    './capital.html',
     './clock.html',
     './syntax.html',
     './words.html',
@@ -23,7 +22,6 @@ const CORE_ASSETS = [
     './manifest-match.json',
     './manifest-life.json',
     './manifest-spark.json',
-    './manifest-capital.json',
     './manifest-syntax.json',
     './manifest-words.json',
     './manifest-jetpack.json',
